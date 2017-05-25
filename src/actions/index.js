32ad311c429e7b9a7ecbf640fb69780a21f48a28
tommy-tran <tome7@live.com>
2017-05-25 17:37:22 -0400

    Connected searchbar, fetch weather and clear after form submits

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const API_KEY = '74f204b599eb48687c44bd5b1b402dfd';
 const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
@@ -6,8 +8,12 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 // Action creator
 export function fetchWeather() {
     const url =`${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+
     return {
-        type: FETCH_WEATHER
+        type: FETCH_WEATHER,
+        payload: request
     };
 
 }
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
     constructor(props) {
         super(props);
 
@@ -15,6 +18,10 @@ export default class SearchBar extends Component {
 
     onFormSubmit(event) {
         event.preventDefault();
+
+        // Fetch weather data
+        this.props.fetchWeather(this.state.term);
+        this.setState({ term: ''});
     }
 
     render() {
@@ -32,3 +39,9 @@ export default class SearchBar extends Component {
         );
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar); // null because this component doesn't care about state
